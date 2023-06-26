9d38cfb61958f1e5871d422b769cde2c585a4368
Bhavik Maheta <18it.bhavik.maheta@gmail.com>
2023-06-26 19:46:01 +0530

    first

---
+++ components/Navbar.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
+
+// import { getCurrentUser } from "@/lib/session";
+
+import AuthProviders from "./AuthProviders";
+import { NavLinks } from "@/constants";
+// import Button from "./Button";
+// import ProfileMenu from "./ProfileMenu";
+
+type NavLinkProps = {
+  href: string;
+  key: string;
+  text: string;
+};
+
+const Navbar = async () => {
+  //   const session = await getCurrentUser()
+  const session = null;
+
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={116} height={43} alt="logo" />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map(({ href, key, text }: NavLinkProps) => (
+            <Link href={href} key={text}>
+              {text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            {/* <ProfileMenu session={session} /> */}
+
+            <Link href="/create-project">
+              {/* <Button title='Share work' /> */}
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
